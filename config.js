e32fff32c6336839c2e523d25e7bcb0ca1884412
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-20 15:55:29 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.ABASTECIMENTO_CONFIG = {
+  // Endereco publico que devolvera as listas actualizadas em JSON.
+  masterDataUrl: "",
+
+  // Endereco que recebera cada submissao do formulario.
+  submissionUrl: "",
+
+  // Enquanto os enderecos estiverem vazios, o site funciona em modo de demonstracao.
+  demoMode: true
+};
