43c905b2cad5934bb7af3cdf73ffc34b65e05aee
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-20 20:01:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.ABASTECIMENTO_CONFIG = {
   masterDataUrl: "",
 
   // Endereco que recebera cada submissao do formulario.
-  submissionUrl: "",
+  submissionUrl: "https://default5fc7bb0c4c3f4008ae039efd62fc96.c9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/14/workflows/821d00349ae0423abd1611a5227b4a48/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RWE_5h6oNG1W0WPqr-gm81dE2LZJ0vYLmkSucksDCVA",
 
   // Enquanto os enderecos estiverem vazios, o site funciona em modo de demonstracao.
   demoMode: true
